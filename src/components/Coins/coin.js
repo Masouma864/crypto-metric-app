a8067f71fcfe659e6e6166e58d9d44a05e981309
masouma <masouma2020@gmail.com>
2022-11-29 22:30:19 +0430

    add header folder and setup babel

---
+++ src/components/Coins/coin.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+
+class coin extends Component {
+    state = {}
+    render() {
+        return (
+
+            <div className="coin-container">
+
+                <div className="main">
+                    <img></img>
+                    <h1>title</h1>
+                </div>
+                <div className="info">
+                    <div>
+                        <span className="title">title</span>
+                        <span className="detail">details</span>
+                    </div>
+                    <div>
+                        <span className="title">title</span>
+                        <span className="detail">details</span>
+                    </div>
+                    <div>
+                        <span className="title">title</span>
+                        <span className="detail">details</span>
+                    </div>
+                    <div>
+                        <span className="title">title</span>
+                        <span className="detail">details</span>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        );
+    }
+}
+
+export default coin;
---
+++ src/components/Coins/coins.js
@@ -0,0 +1,10 @@
+import React from "react";
+import coin from coins
+class coins extends Component {
+    state = {  } 
+    render() { 
+        return ();
+    }
+}
+ 
+export default coins;
