c2aa8c4e4da639021c2e356e0042d397dc3ebbf0
masouma <masouma2020@gmail.com>
2022-11-30 10:17:55 +0430

    fix linter error

--- src/components/Coins/coin.js
+++ src/components/Coins/coin.js
@@ -1,39 +1,39 @@
-import React from 'react';
+// import React from 'react';
 
-class Coin = () => {
+// class Coin = () => {
 
-    return (
+//     return (
 
-        <div className="coin-container">
+//         <div className="coin-container">
 
-            <div className="main">
-                <img alt="photo" />
-                <h1>title</h1>
-            </div>
-            <div className="info">
-                <div>
-                    <span className="title">title</span>
-                    <span className="detail">details</span>
-                </div>
-                <div>
-                    <span className="title">title</span>
-                    <span className="detail">details</span>
-                </div>
-                <div>
-                    <span className="title">title</span>
-                    <span className="detail">details</span>
-                </div>
-                <div>
-                    <span className="title">title</span>
-                    <span className="detail">details</span>
-                </div>
+//             <div className="main">
+//                 <img alt="photo" />
+//                 <h1>title</h1>
+//             </div>
+//             <div className="info">
+//                 <div>
+//                     <span className="title">title</span>
+//                     <span className="detail">details</span>
+//                 </div>
+//                 <div>
+//                     <span className="title">title</span>
+//                     <span className="detail">details</span>
+//                 </div>
+//                 <div>
+//                     <span className="title">title</span>
+//                     <span className="detail">details</span>
+//                 </div>
+//                 <div>
+//                     <span className="title">title</span>
+//                     <span className="detail">details</span>
+//                 </div>
 
-            </div>
+//             </div>
 
-        </div>
+//         </div>
 
-    );
+//     );
 
-}
+// }
 
-export default Coin;
+// export default Coin;
