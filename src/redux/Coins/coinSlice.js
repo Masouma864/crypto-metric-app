1697d875b71b8a6fa68e1244db774f721febba1f
masouma <masouma2020@gmail.com>
2022-11-30 15:09:39 +0430

    create store

--- src/redux/Coins/coinSlice.js
+++ src/redux/Coins/coinSlice.js
@@ -1,17 +1,17 @@
-import { createSlice , createAsycnTunk } from "@reduxjs/toolkit";
+import { createSlice, createAsycnTunk } from '@reduxjs/toolkit';
 
-const fetchDetail = createAsycnTunk('coins/Detail', async(id)=>{
-    const response = await fetch( `https://api.coinstats.app/public/v1/coins/${id}`);
-    const detail = await response.json();
-    const {coin} = detail;
-    return coin;
-})
-const initialState[];
+const fetchDetail = createAsycnTunk('coins/Detail', async (id) => {
+  const response = await fetch(`https://api.coinstats.app/public/v1/coins/${id}`);
+  const detail = await response.json();
+  const { coin } = detail;
+  return coin;
+});
+const initialState = [];
 const detailSlice = createSlice({
-    name:'Detail',
-    initialState,
-    extraReducers:(builder) =>{
-        builder.addCase(fetchDetail.fullfilled,(state,action)=>action.payload);
-    }
-})
- export default detailSlice.reducer;
+  name: 'Detail',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchDetail.fullfilled, (state, action) => action.payload);
+  },
+});
+export default detailSlice.reducer;
