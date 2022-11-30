3a714254d2267480d42049544f5fb215624ef9e3
masouma <masouma2020@gmail.com>
2022-11-30 14:26:18 +0430

    fetch coin assets

---
+++ src/redux/Coins/coinsSlice.js
@@ -0,0 +1,18 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const fetchAssets = createAsyncThunk('coins/FetchAll', async () => {
+  const response = await fetch('https://api.coinstats.app/public/v1/coins/');
+  const assets = await response.json();
+  const { coins } = assets;
+  return coins;
+});
+const initialState = [];
+const coinsSlice = createSlice({
+  name: 'coins',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchAssets.fulfilled, (state, action) => action.payload);
+  },
+
+});
+export default coinsSlice.reducer;
