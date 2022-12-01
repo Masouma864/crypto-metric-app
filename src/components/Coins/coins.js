151e8ce0ac96df9077ff2e809146fa66a70a2409
masouma <masouma2020@gmail.com>
2022-12-01 17:18:58 +0430

    modify coins file

--- src/components/Coins/coins.js
+++ src/components/Coins/coins.js
@@ -1,7 +1,3 @@
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Image from 'react-bootstrap/Row';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
@@ -9,85 +5,66 @@ import { fetchAssets } from '../../redux/Coins/coinsSlice';
 import './Coins.css';
 
 const Coins = () => {
-  const dispatch = useDispatch;
-  const assetList = userSelector((state) => state.coins);
-  useEffect(() => {
-    if (!assetList.length) {
-      [
-        dispatch(fetchAssets()),
-      ];
-    }
-  });
+    const dispatch = useDispatch;
+    const assets = useSelector((state) => state.coins);
+    useEffect(() => {
+        if (!assets.length) {
+            [
+                dispatch(fetchAssets()),
+            ];
+        }
+    });
 
-  const [searchcoin, setSearchcoin] = useState('');
-  const onSeach = (e) => {
-    e.preventDefault();
-    setSearchcoin(e.targer.value);
-  };
+    const [searchcoin, setSearchcoin] = useState('');
+    const onSeach = (e) => {
+        e.preventDefault();
+        setSearchcoin(e.targer.value);
+    };
 
-  if (searchcoin.length > 0) {
-    const searched = assetList.filter(
-      (filteredCoin) => filteredCoin.name.toLowerCase().includes(searchcoin.toLowerCase())
+    if (searchcoin.length > 0) {
+        const searched = assets.filter(
+            (filteredCoin) => filteredCoin.name.toLowerCase().includes(searchcoin.toLowerCase())
                 || filteredCoin.symbol.toLowerCase().includes(searchcoin.toLowerCase()),
-    );
-  }
+        );
+    }
 
-  return (
-    <>
-      <nav className="navbar">
-        <h1>SomeApp</h1>
-      </nav>
-      <Container>
-        <Row>
-          <Col className="welcome">
-            <div className="welcome-body">
-              <p>Welcome!</p>
-              <p>CryptoHouse</p>
+    return (
+
+        <div className="container">
+            <div className="search">
+                <input
+                    type="text"
+                    value={searchcoin}
+                    onChange={onSearch}
+                />
             </div>
-          </Col>
-        </Row>
-        <Row className="filter">
-          <div className="filter-body">
-            <span>Filter:</span>
-            <input
-              type="text"
-              size="sm"
-              value={searchcoin}
-              onChange={onSearch}
-            />
-          </div>
-        </Row>
-        <Row className="cryptos">
-          {searched.map((asset) => (
-            <Col
-              xs={6}
-              className="coll"
-              Key={`${asset.asset_id}${Math.random * 10}`}
-            >
-              <Link
-                className="crypto-card"
-                Key={asset.asset_id}
-                to={`/crypto/${asset.id}`}
-              >
-                <Row className="asset-price">
-                  <p>{asset.price}</p>
-                </Row>
-                <Row>
-                  <Col>
-                    <Image className="icon" src={asset.icon} />
-                  </Col>
-                  <Col className="asset-detail">
-                    <span className="asset-code">{asset.symbol}</span>
-                    <span className="asset-name">{asset.name}</span>
-                  </Col>
-                </Row>
-                <Row />
-              </Link>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </>
-  );
+            <div className="coins">
+                {searched.map((asset) => (
+                    <div
+                        className="coll"
+                        key={`${asset.asset_id}${Math.random * 10}`}
+                    >
+                        <Link
+                            className="coin-card"
+                            Key={asset.asset_id}
+                            to={`/coin/${asset.id}`}>
+                            <div className="price">
+                                <p>{asset.price}</p>
+                            </div>
+                            <div>
+                                <div>
+                                    <img className="icon" src={asset.icon} alt={asset.name} />
+                                </div>
+                                <div className="asset-detail">
+                                    <span className="asset-code">{asset.symbol}</span>
+                                    <span className="asset-name">{asset.name}</span>
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 export default Coins;
