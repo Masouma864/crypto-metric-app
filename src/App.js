d2e9fad1285e9fd42f13492d8cc8ad62f98e86d9
masouma <masouma2020@gmail.com>
2022-12-01 18:25:01 +0430

    make details of coin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Coins from './components/Coins/Coins';
 import Header from './components/Header/header';
+import Coin from './components/Coins/Coin';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Coins />
       <Routes>
         <Route path="/" element={<Coins />} />
+        <Route path="/coin/:id/" element={<Coin />} />
       </Routes>
 
     </div>
--- src/components/Coins/coins.js
+++ src/components/Coins/coins.js
@@ -5,66 +5,67 @@ import { fetchAssets } from '../../redux/Coins/coinsSlice';
 import './Coins.css';
 
 const Coins = () => {
-    const dispatch = useDispatch;
-    const assets = useSelector((state) => state.coins);
-    useEffect(() => {
-        if (!assets.length) {
-            [
-                dispatch(fetchAssets()),
-            ];
-        }
-    });
-
-    const [searchcoin, setSearchcoin] = useState('');
-    const onSeach = (e) => {
-        e.preventDefault();
-        setSearchcoin(e.targer.value);
-    };
-
-    if (searchcoin.length > 0) {
-        const searched = assets.filter(
-            (filteredCoin) => filteredCoin.name.toLowerCase().includes(searchcoin.toLowerCase())
-                || filteredCoin.symbol.toLowerCase().includes(searchcoin.toLowerCase()),
-        );
+  const dispatch = useDispatch;
+  const assets = useSelector((state) => state.coins);
+  useEffect(() => {
+    if (!assets.length) {
+      [
+        dispatch(fetchAssets()),
+      ];
     }
+  });
 
-    return (
+  const [searchcoin, setSearchcoin] = useState('');
+  const onSeach = (e) => {
+    e.preventDefault();
+    setSearchcoin(e.targer.value);
+  };
 
-        <div className="container">
-            <div className="search">
-                <input
-                    type="text"
-                    value={searchcoin}
-                    onChange={onSearch}
-                />
-            </div>
-            <div className="coins">
-                {searched.map((asset) => (
-                    <div
-                        className="coll"
-                        key={`${asset.asset_id}${Math.random * 10}`}
-                    >
-                        <Link
-                            className="coin-card"
-                            Key={asset.asset_id}
-                            to={`/coin/${asset.id}`}>
-                            <div className="price">
-                                <p>{asset.price}</p>
-                            </div>
-                            <div>
-                                <div>
-                                    <img className="icon" src={asset.icon} alt={asset.name} />
-                                </div>
-                                <div className="asset-detail">
-                                    <span className="asset-code">{asset.symbol}</span>
-                                    <span className="asset-name">{asset.name}</span>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </div>
+  if (searchcoin.length > 0) {
+    const searched = assets.filter(
+      (filteredCoin) => filteredCoin.name.toLowerCase().includes(searchcoin.toLowerCase())
+                || filteredCoin.symbol.toLowerCase().includes(searchcoin.toLowerCase()),
     );
+  }
+
+  return (
+
+    <div className="container">
+      <div className="search">
+        <input
+          type="text"
+          value={searchcoin}
+          onChange={onSearch}
+        />
+      </div>
+      <div className="coins">
+        {searched.map((asset) => (
+          <div
+            className="coll"
+            key={`${asset.asset_id}${Math.random * 10}`}
+          >
+            <Link
+              className="coin-card"
+              Key={asset.asset_id}
+              to={`/coin/${asset.id}`}
+            >
+              <div className="price">
+                <p>{asset.price}</p>
+              </div>
+              <div>
+                <div>
+                  <img className="icon" src={asset.icon} alt={asset.name} />
+                </div>
+                <div className="asset-detail">
+                  <span className="asset-code">{asset.symbol}</span>
+                  <span className="asset-name">{asset.name}</span>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Coins;
