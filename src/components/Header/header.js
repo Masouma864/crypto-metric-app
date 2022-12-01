f7eb6e1e846e909f39daa4696822dc7df8876ea4
masouma <masouma2020@gmail.com>
2022-12-01 18:29:40 +0430

    refactor header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,11 +1,29 @@
-import { Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { UilAngleLeft, UilMicrophone, UilSetting } from '@iconscout/react-unicons';
 
 import './header.css';
 
-const Header = () => {
-  <Nav className="navbar">
+const Header = () => (
+  <nav className="navbar">
     <h1>Moneyster</h1>
-  </Nav>;
-};
+  <nav className="navbar flex">
+    <div className="back">
+      <Link to="/">
+        <UilAngleLeft />
+      </Link>
+    </div>
+    <div className="app-title">
+      <span>Moneyster</span>
+    </div>
+    <div className="app-icons flex">
+      <div className="mic">
+        <UilMicrophone />
+      </div>
+      <div className="setting">
+        <UilSetting />
+      </div>
+    </div>
+  </nav>
+);
 
 export default Header;
